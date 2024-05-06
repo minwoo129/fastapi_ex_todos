856ae85dfe38158c15067eacc31c913dea202a26
minwoo129 <key0129mw@naver.com>
2024-05-06 10:41:30 +0900

    feat: orm, repository 추가, api 호출 부분 수정

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
-from fastapi import FastAPI, Body, HTTPException
+from typing import List
+
+from fastapi import FastAPI, Body, HTTPException, Depends
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from database.connection import get_db
+from database.repository import get_todos
+
+from database.orm import ToDo
 
 app = FastAPI()
 
@@ -26,11 +34,14 @@ todo_data = {
 }
 
 @app.get("/todos", status_code=200)
-def get_todos_handler(order: str | None = None):
-    ret = list(todo_data.values())
+def get_todos_handler(
+        order: str | None = None,
+        session: Session = Depends(get_db)
+):
+    todos: List[ToDo] = get_todos(session)
     if order and order == "DESC":
-        return ret[::-1]
-    return ret
+        return todos[::-1]
+    return todos
 
 @app.get("/todos/{todo_id}", status_code=200)
 def get_todo_handler(todo_id: int):
